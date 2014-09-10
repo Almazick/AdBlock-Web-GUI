87f56f76184da45f42381efe921a5547f657164a
Almazick <Almazick@gmail.com>
2014-09-10 04:40:54 -0500

    Almazick

--- ads.sh
+++ ads.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 # AdBlock Web GUI by Almaz
-# Version: 1.3
+# Version: 1.31
 # Using Tomato firmware just put all the files in /var/wwwext/
 ##############################################################################
 adblockpath="/var/wwwext/adblock.sh"  	# location of adblock.sh by Jerrm
-pixelservip="192.168.1.254"				# Pixelserv IP address		
+pixelservip="192.168.3.254"				# Pixelserv IP address		
 scriptname="ads.sh"						# this script name
 dnsmasqlog="/tmp/var/log/messages*"		# dnsmasq log location, by default it's using syslog
-tmpfolder="/tmp"						# location of your temp folder
-dnsmasq_external_log="n"				# for external dnsmasq log then enter "y" otherwise "n" for default syslog
-##############################################################################
-if grep -q 'echo $(wc -l < "$blocklist") > /tmp/adscount' $adblockpath
+tmpfolder="/tmp"						# location of your temp folder 
+dnsmasq_external_log="n"				# If you are using external dnsmasq log then enter "y" otherwise "n" for default syslog
+######################################################
+if grep -q 'adscount' $adblockpath
 then
 echo ""
 else
-sed '/elog "$(wc -l < "$blocklist") unique hosts to block"/ a \echo $(wc -l < "$blocklist") > $tmpfolder/adscount' $adblockpath > $tmpfolder/tmp090; mv $tmpfolder/tmp090 $adblockpath
+sed '/elog "$(wc -l < "$blocklist") unique hosts to block"/ a \echo $(wc -l < "$blocklist") > dummyname5/adscount' $adblockpath > $tmpfolder/tmp090; mv $tmpfolder/tmp090 $adblockpath
+sed -i "s|dummyname5|"$tmpfolder"|g" $adblockpath
 chmod +x $adblockpath
 fi
 REFRESHTIME=60
